9b9c259f2e7d8cf6b958248e05f9d925797a9862
Samuel <samnmbuguah@gmail.com>
2023-04-05 08:37:10 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+//Initializing variables for HTML elements
+const errorModal = document.querySelector("#modal");
+const likeButtons = document.querySelectorAll(".like-glyph");
+const errorMessage = document.querySelector("#modal-message");
 
-// Your JavaScript code goes here!
-
-
-
+//Loop through all the items in the likeButtons colection
+likeButtons.forEach(button => {
+  button.addEventListener("click", () =>{
+    mimicServerCall()
+    .then(serverResponse => {
+      if(button.textContent === EMPTY_HEART){
+        errorModal.classList.add("hidden");
+        console.log(serverResponse);
+        button.textContent = FULL_HEART;
+        button.classList.add("activated-heart")
+    } else {
+        button.textContent = EMPTY_HEART;
+        button.classList.remove("activated-heart");
+    }
+    })
+    .catch(error => {
+      errorMessage.textContent = error;
+      console.log(error);
+      errorModal.classList.remove("hidden");
+      button.classList.remove("activated-heart");
+      button.textContent = EMPTY_HEART;
+      if(errorModal.classList !== "hidden" ){
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+    }
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -23,3 +52,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+console.log(mimicServerCall());
